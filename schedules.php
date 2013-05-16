7043052b1bd6a5f5ab0325f30488739e82b6ab43
Aaron Martin <sbbeachvball@gmail.com>
2013-05-16 16:00:43 -0700

    little bit of data driven work for the mobile framework

--- schedules.php
+++ schedules.php
@@ -16,50 +16,90 @@
 
 
 <?php
-  $statearray = array(
-      'ca' => "California",
-      'wa' => "Washington",
-      'nv' => "Nevada",
-      'or' => "Oregon",
-  );
 
-  $sitesarray = array(
+function topmenu($label = ""){
+    print "<div class=\"menu-full menu-detailed menu-padded\">\n";
+    print "<h1 class=\"light menu-first\">$label</h1>\n"; 
+    print "<ol> \n";
+    
+}
+
+
+$statearray = array(
+    'ca' => "California",
+    'wa' => "Washington",
+    'nv' => "Nevada",
+    'or' => "Oregon",
+    );
+
+$sitesarray = array(
     'ca' => array("Agoura Hills","Goleta", "Ojai","Ventura"),
     'wa' => array("Seattle","Tacoma"),
     'or' => array("Salem"),
     'nv' => array("Reno"),
-  );
+    );
+$programarray = array(
+    'Agoura Hills' => array(
+        "Game1 - 10:00AM Court 3 vs. Amazing Hamsters",
+        "Game2 - 10:30AM Court 5 vs. Gophers It",
+        "Game3 - 11:00AM Court 4 vs. Rapping Rats",
+        "Game4 - 11:30AM Court 3 vs. Minnies Mice"),
+    'Goleta' => array(
+        "Game1 - 10:00AM Court 3 vs. Amazing Hamsters",
+        "Game2 - 10:30AM Court 5 vs. Gophers It",
+        "Game3 - 11:00AM Court 4 vs. Rapping Rats",
+        "Game4 - 11:30AM Court 3 vs. Minnies Mice"),
+    'Oxnard' => array(
+        "Game1 - 10:00AM Court 3 vs. Amazing Hamsters",
+        "Game2 - 10:30AM Court 5 vs. Gophers It",
+        "Game3 - 11:00AM Court 4 vs. Rapping Rats",
+        "Game4 - 11:30AM Court 3 vs. Minnies Mice"),
+    'Ventura' => array(
+        "Game1 - 10:00AM Court 3 vs. Amazing Hamsters",
+        "Game2 - 10:30AM Court 5 vs. Gophers It",
+        "Game3 - 11:00AM Court 4 vs. Rapping Rats",
+        "Game4 - 11:30AM Court 3 vs. Minnies Mice"),
+    );
 
-  if (isset($_GET['state'])){
-  $sh = $_GET['state'];
+if (isset($_GET['program'])){
+    $program = $programarray[$_GET['program']];
+}
 
-?>
-<div class="menu-full menu-detailed menu-padded">
-    <h1 class="light menu-first">Locate your USYVL Program/Site</h1> 
-    <ol> 
 
-<?php
-    foreach( $sitesarray[$sh] as $program){
-      print "      <li>\n";
-      print "        <a href=\"./schedules.php?state=$sh&program=$program\">\n";
-      print "        $program\n";
-      print "      </a></li>\n";
+if (isset($_GET['state'])){
+    $sh = $_GET['state'];
+    
+    if( ! isset($program)){
+        
+        topmenu("Locate your USYVL Program/Site");
+        
+        foreach( $sitesarray[$sh] as $program){
+            print "      <li>\n";
+            print "        <a href=\"./schedules.php?state=$sh&program=$program\">\n";
+            print "        $program\n";
+            print "      </a></li>\n";
+        }
     }
-  }
-  else {
-?>
-<div class="menu-full menu-detailed menu-padded">
-    <h1 class="light menu-first">Locate your USYVL Program/Site</h1> 
-    <ol> 
+    else {
+        topmenu("Your Schedule");
+        foreach( $program as $item){
+            print "      <li>\n";
+            print "        <a href=\"./schedules.php?state=$sh&program=$program\">\n";
+            print "        $item\n";
+            print "      </a></li>\n";
+        }
+    }
+}
 
-<?php
+else {
+    topmenu("Locate your USYVL Program/Site");
     foreach( $statearray as $sh => $fullstate){
-      print "      <li>\n";
-      print "        <a href=\"./schedules.php?state=$sh\">\n";
-      print "        $fullstate\n";
-      print "      </a></li>\n";
+        print "      <li>\n";
+        print "        <a href=\"./schedules.php?state=$sh\">\n";
+        print "        $fullstate\n";
+        print "      </a></li>\n";
     }
-  }
+}
 ?>
     </ol>
 </div>
