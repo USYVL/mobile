450e0946d0cd303c6f6fb72b772d564c21b5759e
Aaron Martin <sbbeachvball@gmail.com>
2013-06-15 14:26:17 -0700

    significant updates to scorekeeper

--- js/scorekeeper.js
+++ js/scorekeeper.js
@@ -1,3 +1,277 @@
 if ($()) {
     //alert("Hi There");
 }
+
+// allowed scoreTypes
+// Rally
+// DoubleMax
+// HotPotato
+// SideOut
+$(function() {
+        var scoreTypes = [ "DoubleMax","HotPotato","Rally","SideOut"];
+        var len = scoreTypes.length;
+        var scoreMax = 25;
+        var winByTwo = true;        
+        var gameOver = false;
+        var scoreType  = "DoubleMax"; // type of scoring - only rally currently supported
+        var scoreCap = 35;  // where we no longer need to win by two
+        var switchOn = 7;  // indicate a side change on multiples of this 
+        var switchAt = 0;  // indicate a single switch in the middle of the game
+        
+        var $tmA = $('#tmA');
+        var $tmB = $('#tmB');
+        var $tmAminus = $('#tmA_minus');
+        var $tmBminus = $('#tmB_minus');
+        var $serve = $('#toggle_serve');
+        var tmAscore = parseInt($tmA.html());
+        var tmBscore = parseInt($tmB.html());
+        var tmAcons = 0;
+        var tmBcons = 0;
+        var notes = "";
+        
+        function updateNotes(){
+            notes="Scoring Type: "+scoreType+"<br />\n";
+            notes+="Score Max: "+scoreMax+"<br />\n";
+            notes+="Score Cap: "+scoreCap+"<br />\n";
+            notes+="Win By Two: ";
+            notes+=(winByTwo)?"Yes":"No";
+            notes+="<br />\n";
+            notes+="Side Change On: "+switchOn.toString()+"<br />\n";
+            notes+="Side Change At: "+switchAt.toString()+"<br />\n";
+            $('#notes').html(notes);
+        }
+        updateNotes();
+        
+        
+        // 
+        $tmA.click(function(){
+                if( gameOver ) return;
+                servStatus='#tmA_service';
+                
+                var iServed = $(servStatus).isVisible();
+                if( iServed) tmAcons++;
+                else         {
+                    tmAcons=0;
+                    //tmBcons++;  // pretty sure this breaks doublemax
+                }
+                
+                //var tmAscore = parseInt($tmA.html()) + 1;
+                if( scoreType == "SideOut") {
+                    if(  iServed ) tmAscore++;
+                }
+                else     tmAscore++;
+            
+                $tmA.html(tmAscore.toString());
+                checkControls(tmAscore,tmBscore);
+                checkService(servStatus,tmAcons,tmBcons);
+                //if ($('#tmB_service').isVisible()){
+                //    serviceChange();
+                //}
+                checkScore(tmAscore,tmBscore);
+                checkSideChange(tmAscore,tmBscore);
+        });
+        $tmB.click(function(){
+                if( gameOver ) return;
+                servStatus='#tmB_service';
+                var iServed = $(servStatus).isVisible();
+                
+                if( iServed) tmBcons++;
+                else         {
+                    tmBcons=0;
+                    //tmAcons++;  // pretty sure this breaks doublemax
+                }
+                
+                if( scoreType == "SideOut") {
+                    if(  iServed ) tmBscore++;
+                }
+                else     tmBscore++;
+            
+                //var tmBscore = parseInt($tmB.html()) + 1;
+                $tmB.html(tmBscore.toString());
+                checkControls(tmAscore,tmBscore);
+                checkService(servStatus,tmBcons,tmAcons);
+                checkScore(tmAscore,tmBscore);
+                checkSideChange(tmAscore,tmBscore);
+                //if ($('#tmA_service').isVisible()){
+                //    serviceChange();
+                //}
+        });
+        $tmAminus.click(function(){
+                if( gameOver ) gameOver = false;
+                tmAscore--;
+                //var tmAscore = parseInt($tmA.html()) - 1;
+                $tmA.html(tmAscore.toString());
+                checkControls(tmAscore,tmBscore);
+        });
+        $tmBminus.click(function(){
+                if( gameOver ) gameOver = false;
+                //var tmBscore = parseInt($tmB.html()) - 1;
+                tmBscore--;
+                $tmB.html(tmBscore.toString());
+                checkControls(tmAscore,tmBscore);
+        });
+        
+        $('#scoreType').click(function () {
+                // cycle through the options
+                // first find which one we are currently set for:
+                scoreType = $(this).html();
+                for( var i=0; i<len; i++){
+                    if( scoreType == scoreTypes[i] ){
+                        nexti = (i+1)%len;
+                    }
+                }
+                scoreType = scoreTypes[nexti];
+                $('#scoreType').html(scoreType);
+                updateNotes();
+                
+        });
+        
+        $('#switch_sides').click(function (){
+                $('#c1_wrapper').toggleClass('lfloat');
+                $('#c1_wrapper').toggleClass('rfloat');
+                $('#c2_wrapper').toggleClass('lfloat');
+                $('#c2_wrapper').toggleClass('rfloat');
+        });
+        
+        // get scores and see if there is a winner
+        //$tmAscore = parseInt($tmA.html());
+        //$tmBscore = parseInt($tmB.html());
+        
+        function checkControls(a,b) {
+            if( a == 0 && b == 0){
+                $serve.show();
+                $('#scoreType').show();
+            }
+            else {
+                $serve.hide();
+                $('#scoreType').hide();
+            }
+        }
+        function checkScore(a,b){
+            if( a >= scoreMax || b >= scoreMax ){
+                if( a >= scoreCap || b >= scoreCap && winByTwo ){
+                    winByTwo = false;
+                }
+                if( winByTwo ){
+                    if( Math.abs(a-b) >= 2){
+                        declareWinner(a,b);
+                    }
+                    else {
+                        $('#winner').html("Need to Win by two");
+                    }
+                }
+                else {
+                    declareWinner(a,b);
+                }
+            }
+        }
+        function declareWinner(a,b){
+            gameOver = true;
+            if( a > b ){
+                var name = $('#tmAname').html();
+            }
+            else {
+                var name = $('#tmBname').html();
+            }
+            $('#winner').html(name+" Wins!");
+        }
+        
+        // check to see if service needs to change
+        // the choice of when to switch depends on what type of scoring is used
+        //  hot-potato - two serves regardless of results
+        //  rally scoring - team that gets the point serves
+        //  double-max - modified rally scoring, team that gets point gets up to two serves
+        //  sideout - points only awarded if serving, winner of rally serves
+        
+        // This function currently does rally scoring
+        function checkService(which,cons1,cons2){
+            switch(scoreType)
+            {
+            case "Rally":
+                rallyService(which);
+                break;
+            case "DoubleMax":
+                doubleMaxService(which,cons1);
+                break;
+            case "HotPotato":
+                hotPotatoService(which);
+                break;
+            case "SideOut":
+                sideOutService(which);
+                break;
+            default:
+                doubleMaxService(which,cons1);
+                
+            }
+        }
+        function rallyService(which){
+            if (! $(which).isVisible()){
+                serviceChange();
+            }
+        }
+        function hotPotatoService(which){
+            // uses the globals, not super elegant
+            if( cons1>=2 || cons2>=2 ){
+                serviceChange();
+                tmAcons=0;
+                tmBcons=0;
+            }
+                //if( cons2>=2 ) serviceChange();
+            //}
+            //else serviceChange();
+        }
+        function checkSideChange(a,b){
+            if(switchOn>0){
+                if( ((a+b)%switchOn) == 0){
+                    $('#switch_sides').addClass("sideChangeAlert");
+                    $('#switch_sides').fadeIn(100).fadeOut(100).delay(100).fadeIn(100).fadeOut(100).delay(100).fadeIn(100);
+                }
+                else {
+                    $('#switch_sides').removeClass("sideChangeAlert");
+                }
+            }
+            if(switchAt>0){
+                if((a+b) == switchAt){
+                   $('#switch_sides').addClass("sideChangeAlert");
+                    $('#switch_sides').fadeIn(100).fadeOut(100).delay(100).fadeIn(100).fadeOut(100).delay(100).fadeIn(100);
+                }
+                else {
+                    $('#switch_sides').removeClass("sideChangeAlert");
+                }
+            }
+        }
+        function sideOutService(which){
+            if (! $(which).isVisible()){
+                serviceChange();
+            }
+        }
+        
+        // doubleMax is a modified rally scoring.
+        // point is awarded to winner of each rally
+        // but a team is allowed no more than 2 serves in a row
+        // so even if the serving team wins the rally on its second serve, service goes to the other team
+        function doubleMaxService(which,cons){
+            if ($(which).isVisible()){
+                if( cons>=2 ) serviceChange();
+            }
+            else serviceChange();
+        }
+        
+        function serviceChange(){
+            $('#tmA_service').toggle();
+            $('#tmB_service').toggle();
+        }
+        
+        $serve.click(function() {
+            serviceChange();
+        });
+        
+        $.fn.isVisible = function() {
+            return $.expr.filters.visible(this[0]);
+        };
+        
+});
+
+$(function() {
+});
+
--- scorekeeper.php
+++ scorekeeper.php
@@ -8,235 +8,26 @@ $content['errs'] = "";
 $content['title'] = "";
 $content['body'] = "";
 
-/***
-class mwfMobileSite {
-    function __construct(){
-        $this->mode = "";
-        $this->title = "Main Menu";
-        $this->processGET();
-    }
-    function processGET(){
-        if( isset($_GET['mode'])) $this->mode = $_GET['mode'];
-    }
-    function dispMain(){
-        $b = "";
-        $b .= $this->topmenu("Main Menu");
-        $b .= "  <li><a href=\"" . $_SERVER['PHP_SELF'] . "?mode=states\">Get Your Schedule</a></li>\n";
-        $b .= "  <li><a href=\"" . $_SERVER['PHP_SELF'] . "?mode=auto\">Auto Mode</a></li>\n";
-        $b .= "  <li><a href=\"" . $_SERVER['PHP_SELF'] . "?mode=score\">Score Keeper</a></li>\n";
-        $b .= "</ol>\n";
-        $b .= "</div> <!-- close top menu div -->\n\n";
-
-
-        //$b .= "<br />\n";
-        //$b .= "<div class=\"light padded\"><!-- test div -->\n";
-        $b .= "<div class=\"content-padded content-full\">\n";
-        $b .= "<h2 class=\"light\">Content</h2>\n";
-        //$b .= "  <div>\n";
-        $b .= "    <p>\n";
-        $b .= "    This is a very preliminary version of the mobile site.\n";
-        $b .= "    </p>\n";
-        $b .= "    <p>\n";
-        $b .= "    Its designed to allow mobile access to schedules during the course of the season.\n";
-        $b .= "    </p>\n";
-        //$b .= "  </div>\n";
-        $b .= "</div>\n";
-        //$b .= "</div> <!-- close of test div -->\n";
-        return "$b";
-    }
-    function dispStates(){
-        $sdb = $GLOBALS['dbh']['sdb'];
-        $this->title = "USYVL Mobile - Select State";
-        
-        $b = "";
-        $b .= $this->topmenu("Select State");
-        $b .= "<ul>\n";
-        $states = $sdb->fetchList("distinct evstate from ev");
-        foreach( $states as $state){
-           $b .= "  <li><a href=\"" . $_SERVER['PHP_SELF'] . "?mode=programs&state=$state\">$state</a></li>\n";
-        }
-        
-        $b .= "</ul>\n";
-        return "$b";
-    }
-    function dispPrograms(){
-        $sdb = $GLOBALS['dbh']['sdb'];
-        $state = $_GET['state'];
-        $this->title = "USYVL Mobile - Select Program from $state";
-        
-        $b = "";
-        $b .= $this->topmenu("Select Program");
-        $b .= "<ul>\n";
-        $programs = $sdb->fetchList("distinct evprogram from ev","evstate='" . $_GET['state'] . "'");
-        foreach( $programs as $program){
-           $b .= "  <li><a href=\"" . $_SERVER['PHP_SELF'] . "?mode=divisions&state=$state&program=$program\">$program</a></li>\n";
-        }
-        
-        $b .= "</ul>\n";
-        return "$b";
-    }
-    function dispDivisions(){
-        $sdb = $GLOBALS['dbh']['sdb'];
-        $state = $_GET['state'];
-        $program = $_GET['program'];
-        $this->title = "USYVL Mobile - Select Division from $state Program $program";
-        
-        $b = "";
-        $b .= $this->topmenu("Select Age Division");
-        $b .= "<ul>\n";
-        $divisions = $sdb->fetchList("distinct tmdiv from tm","tmprogram='" . $_GET['program'] . "'");
-        foreach( $divisions as $division){
-           $b .= "  <li><a href=\"" . $_SERVER['PHP_SELF'] . "?mode=teams&division=$division&state=$state&program=$program\">$division</a></li>\n";
-        }
-        
-        $b .= "</ul>\n";
-        return "$b";
-    }
-    function dispTeams(){
-        $sdb = $GLOBALS['dbh']['sdb'];
-        $state = $_GET['state'];
-        $program = $_GET['program'];
-        $division = $_GET['division'];
-        $this->title = "USYVL Mobile - Select Team in $division Division from $state Program $program";
-        
-        $b = "";
-        $b .= $this->topmenu("Select Team");
-        $b .= "<ul>\n";
-        //$teams = $sdb->fetchList("distinct name from tm","program='" . $_GET['program'] . "' and div='" . $division . "'");
-        $data = $sdb->getKeyedHash('tmid',"select * from tm where tmprogram='" . $_GET['program'] . "' and tmdiv='" . $division . "'");
-        foreach( $data as $k => $d){
-            $team = $d['tmname'];
-            $tmid = $k;
-           $b .= "  <li><a href=\"" . $_SERVER['PHP_SELF'] . "?mode=sched&tmid=$tmid&team=$team&division=$division&state=$state&program=$program\">$team</a></li>\n";
-        }
-        
-        $b .= "</ul>\n";
-        return "$b";
-    }
-    function dispSched(){
-        $sdb = $GLOBALS['dbh']['sdb'];
-        $state = $_GET['state'];
-        $program = $_GET['program'];
-        $division = $_GET['division'];
-        $team = $_GET['team'];
-        $tmid = $_GET['tmid'];
-        $this->title = "USYVL Mobile - $team Schedule";
-        
-        $b = "";
-        //$b .= $this->topmenu("$team Schedule");
-        $b .= "<div class=\"content-padded content-full\">\n";
-        $b .= "<h1>$team Schedule</h2>\n";
-        //$b .= "<ul>\n";
-        
-        // This first part deals with practices that are NOT in the sched db (at this time)..
-        // select * from ev left outer join tm on ev.program = tm.program where ev.name like '%Practice%' and tm.tmid = '231';
-        //$prac = $sdb->getKeyedHash('evid',"select * from ev left outer join tm on evprogram = tmprogram where evname like '%Practice%' and tmid = $tmid");
-        //foreach( $prac as $d){
-        //    $date = $d['evds'];
-        //    $court = $d['evcourt'];
-        //    $bt = $d['evtime_beg'];
-        //    $et = $d['evtime_end'];
-        //    $b .= "<li>$date - $bt - $et - Ct$court - Practice</li>\n";
-        //}
-        
-        // need to get team id
-        $data = $sdb->getKeyedHash('gmid',"select * from gm left join ev on gm.evid = ev.evid where tmid1 = $tmid or tmid2 = $tmid order by evds");
-        foreach( $data as $d){
-            $evloc = $d['evlocation'];
-            $evnm = $d['evname'];
-            $date = $d['evds'];
-            $court = $d['court'];
-            //$bt = $d['evtime_beg'];
-            //$et = $d['evtime_end'];
-            $time = $d['time'];
-            // depending on type of event, court may be specified in one of two locations...
-            $b .= "<p>\n";
-            $b .= "$date - $time<br />";
-            $b .= "$evnm<br />\n";
-            $b .= "$evloc - Court $court\n";
-            $b .= "</p>\n";
-            //$b .= "  <li><a href=\"" . $_SERVER['PHP_SELF'] . "?mode=sched&team=$team&division=$division&state=$state&program=$program\">$team</a></li>\n";
-        }
-        $b .= "</div>\n";
-        //$b .= "</ul>\n";
-        return "$b";
-    }
-    function display(){
-        global $content;
-        $b = "";
-        //$b .= "Calling display(mode: " . $this->mode . "):<br />";
-
-        //$content['errs'] .= "Mode: " . $this->mode . "\n";
-        
-        switch ($this->mode){
-            case "select" :
-                $b .= $this->dispSelect();
-                break;
-            case "states" :
-                $b .= $this->dispStates();
-                break;
-            case "programs" :
-                $b .= $this->dispPrograms();
-                break;
-            case "divisions" :
-                $b .= $this->dispDivisions();
-                break;
-            case "teams" :
-                $b .= $this->dispTeams();
-                break;
-            case "sched" :
-                $b .= $this->dispSched();
-                break;
-            default :
-                $b .= $this->dispMain();
-                break;
-        }
-                
-        $b .= $this->button("http://www.usyvl.org","USYVL Website");
-        $b .= $this->button("./","Main Menu");
-        return "$b";
-    }
-    function topmenu($label = ""){
-        $b = "";
-        $b .=  "<div class=\"menu-full menu-detailed menu-padded\">\n";
-        $b .=  "<h1 class=\"light menu-first\">$label</h1>\n"; 
-        $b .=  "<ol> \n";
-        //$b .=  "<ol> \n";
-        return $b;
-        
-    }
-    function button($href = "", $label = ""){
-        $b = "";
-        $b .= "<a href=\"" . $href . "\" class=\"button-full button-padded\">$label</a>\n";
-        return $b;
-    }
-    function getTitle(){
-        return $this->title;
-    }
-}
-
-**/
-
-//$ms = new mwfMobileSite();
-//$ms->processGET();
 $b .= '<div id="skwrapper">';
 $htb = '
 
-  <div id="c1_wrapper">
+  <div id="c1_wrapper" class="lfloat">
     <div class="section_style">
-      <p class="team_label">Home</p>
-      <button id="hometeam" class="score">25</button>
-      <button id="hometeam_minus" class="decrement">decrement</button>  
+      <p id="tmAname" class="team_label">Team A</p>
+      <button id="tmA" class="score">0</button>
+        <button id="tmA_minus" class="decrement">-</button>  
+        <span id="tmA_service" class="service">Serving</span>  
     </div>
   </div>
   ';
   
   
-$atb .= '<div id="c2_wrapper">
+$atb .= '<div id="c2_wrapper" class="rfloat">
     <div class="section_style">
-      <p class="team_label">Visitor</p>
-      <button id="awayteam" class="score">25</button>  
-      <button id="awayteam_minus" class="decrement">decrement</button>  
+      <p id="tmBname" class="team_label">Team B</p>
+      <button id="tmB" class="score">0</button>  
+        <button id="tmB_minus" class="decrement">-</button>  
+        <span id="tmB_service" class="service hide">Serving</span>  
     </div>
   </div>
 ';
@@ -244,32 +35,25 @@ $atb .= '<div id="c2_wrapper">
 $b .= "$htb";
 $b .= "$atb";
 
+$b .= '<div class="clear"></div><!-- clear floats -->';
+
+$b .= '<div id="winner"></div>';
+$b .= '<button id="switch_sides">Switch Sides</button>';
+$b .= '<button id="toggle_serve">Toggle Initial Service</button>';
+$b .= '<button id="scoreType">DoubleMax</button>';
+$b .= '<div id="notes"></div>';
+
 $b .= '</div><!-- close skwrapper -->
-<div class="clear">
-</div>
 ';
+
+$b .= '<div id="notes" class="content-padded"></div>';
 
 $content['body'] .= "$b";
 $content['title'] = "ScoreKeeper";  // title is not set till after display is run...
 $content['errs'] .= "";
 
-//ob_start();
 include("tpl/scorekeeper.tpl");
-//print ob_get_clean();
 
-
-// Need to figure out a clever way to determine how much information we have
-/*
-How to handle DB, dont need full dbmgmt pkg as we dont need to write to or build this db
-
-Punch through the following levels:
-season  # can just take whatever db we have...
-state
-program
-division
-team     # get tmidmake
-day  or display all days
-*/
 
 ?>
 
