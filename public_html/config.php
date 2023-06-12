78b1b08b3f4b25d497c7a1d7359218eee52b9fd9
Aaron Martin <sbbeachvball@gmail.com>
2023-06-12 15:05:19 -0700

    minor updates to config.php

--- public_html/config.php
+++ public_html/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('MOBILE_SCHED_DB','sqlite:/var/www/usyvl-repos/scheduling/public_html/io-data/db/sched.sqlite3');
-define('REDBOOK_MANUAL_DB','sqlite:/var/www/usyvl-repos/scheduling/public_html/io-data/db/redbook.sqlite3');
+define('MOBILE_SCHED_DB','/var/www/usyvl-repos/scheduling/public_html/io-data/db/sched.sqlite3');
+define('REDBOOK_MANUAL_DB','/var/www/usyvl-repos/scheduling/public_html/io-data/db/redbook.sqlite3');
 
 ini_set('include_path',ini_get('include_path') . ":./inc:../inc");
 
